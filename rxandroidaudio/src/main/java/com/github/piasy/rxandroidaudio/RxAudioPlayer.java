dc980c1447aa23bc60083b0ac5490907a5f88075
Lucas Nelaupe <lucas34990@gmail.com>
2016-08-30 00:06:49 +0800

    Resole #9 Expose media player

--- rxandroidaudio/src/main/java/com/github/piasy/rxandroidaudio/RxAudioPlayer.java
+++ rxandroidaudio/src/main/java/com/github/piasy/rxandroidaudio/RxAudioPlayer.java
@@ -47,7 +47,7 @@ public final class RxAudioPlayer {
 
     static final String TAG = "RxAudioPlayer";
 
-    MediaPlayer mPlayer;
+    private MediaPlayer mPlayer;
 
     private RxAudioPlayer() {
         // singleton
@@ -363,6 +363,10 @@ public final class RxAudioPlayer {
         }
         mPlayer = null;
         return true;
+    }
+
+    public MediaPlayer getMediaPlayer() {
+        return mPlayer;
     }
 
     private static class RxAudioPlayerHolder {
